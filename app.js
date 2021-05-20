294882d7c7ac5965d1421917d727bf577f0c5297
soflarity <55975006+soflarity@users.noreply.github.com>
2021-05-20 14:20:20 +0100

    Add App file

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const app = require('express');
-
+//Dependencies
+const express = require('express');
+const app = express();
 
 // Middleware
+app.use(express.static(__dirname + '/Public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html');
+})
+
+app.listen(3000, () => {
+    console.log('Maze Game running on port 3000!')
+})
